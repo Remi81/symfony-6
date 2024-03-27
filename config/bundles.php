5da09b3e222fdcb49773f6d731cd8646913a2076
Remi BARRET <barret.remi.pro@gmail.com>
2024-03-27 18:01:18 +0100

    nelmio cors

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
